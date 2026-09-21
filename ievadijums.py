5b7971c64e7d89018f444d2a60c13cd6e501a0c8
Darina <lobacevad7@gmail.com>
2026-09-21 09:34:10 +0300

    Tagad ir 2 receptes

--- ievadijums.py
+++ ievadijums.py
@@ -1,5 +1,12 @@
 cukura_cena=float(input("Īevadi cukura cenu :"))
 abola_kg=float(input("Īevadi cukura skaits kilogramos :"))
 print(f"par cukuru tu samaksāsi {cukura_cena*abola_kg*0.7}eiro")
-
+if recept == 1
+cukura_cena=float(input("Īevadi cukura cenu :"))
+abola_kg=float(input("Īevadi cukura skaits kilogramos :"))
+print(f"par cukuru tu samaksāsi {cukura_cena*abola_kg*0.5}eiro")
+ else recept == 2
+cukura_cena=float(input("Īevadi cukura cenu :"))
+abola_kg=float(input("Īevadi cukura skaits kilogramos :"))
+print(f"par cukuru tu samaksāsi {cukura_cena*abola_kg}eiro")
 
